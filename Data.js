847c1d5554fe81df52b8fd82d6c1902f9057a642
Tomek Matras <tomasz.matras@developerweb.pl>
2020-05-01 16:35:14 +0200

    add fn select

--- Data.js
+++ Data.js
@@ -1,11 +1,13 @@
 class Data {
   constructor() {
     this.arrows = [...document.querySelectorAll(".img__arrow")];
+    this.info = document.querySelector(".wrapper__info");
     this.pictures = [
       { img: "/img/1.jpg" },
       { img: "/img/2.jpg" },
       { img: "/img/3.jpg" },
       { img: "/img/4.jpg" },
     ];
+    this.counter = 0;
   }
 }
--- Slider.js
+++ Slider.js
@@ -1,18 +1,35 @@
 class Slider extends Data {
   constructor(props) {
     super(props);
-    this.showDiv = document.querySelector(".img");
+    this.showDiv = document.querySelector(".img__img");
     this.arrows.forEach((arrow) =>
       arrow.addEventListener("click", this.select.bind(this))
     );
   }
   select(e) {
     if (e.target === this.arrows[1]) {
-      console.log("right");
+      if (this.counter < this.pictures.length - 1) {
+        this.counter++;
+        this.showDiv.src = this.pictures[this.counter].img;
+        this.info.textContent = `${this.counter + 1} / ${this.pictures.length}`;
+      } else if (this.counter === this.pictures.length) {
+        this.counter = this.counter;
+      }
     } else if (e.target === this.arrows[0]) {
-      console.log("left");
+      if (this.counter === 0) {
+        this.counter = this.counter;
+      } else if (this.counter > 0) {
+        this.counter--;
+        this.showDiv.src = this.pictures[this.counter].img;
+        this.info.textContent = `${this.counter + 1} / ${this.pictures.length}`;
+      }
     }
+    this.arrowsShow();
   }
+  arrowsShow = () => {
+    if (this.counter === 0) {
+    }
+  };
 }
 
 const slider = new Slider();
